7ccf8a57fdda0c640dcbab473923d77b33333550
Alex <canec-007@bk.ru>
2020-01-25 18:53:07 +0300

    added new class "cell"

--- main.cpp
+++ main.cpp
@@ -1,8 +1,43 @@
 #include <iostream>
 using namespace std;
 
+// all cells in game field - objects
+class cell
+{
+  int RC;
+  int CC;
+  string status; // wall, start, finish, question, free, undef
+  cell *right, *left, *top, *bot;
+
+public:
+
+  // add new cell funcrion
+  void addCell()
+  {
+
+  }
+
+  // def constructor
+  cell()
+  {
+    RC = CC = -1;
+
+    right = left = top = bot = NULL;
+    status = "undef";
+  }
+
+  // destructor
+  ~cell()
+  {
+    delete right;
+    delete left;
+    delete top;
+    delete bot;
+  }
+};
+
 int main()
 {
-
+  cout << "Hello world" << endl;
   return 0;
 }
